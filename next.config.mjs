76d417deed6d9a80568041bf84c57fe2930345e5
madushan10 <mnmnjith@gmail.com>
2025-08-06 22:04:19 +0530

    Revert "update4"
    This reverts commit 0ae0b23256ea67f01f23dd8dcf191b85cd4e592f.

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
     ],
   },
 
-async headers() {
+  async headers() {
   return [
     {
       source: '/(.*)',
@@ -26,26 +26,13 @@ async headers() {
         { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
         { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
         { key: 'X-Content-Type-Options', value: 'nosniff' },
-        {
-          key: 'Content-Security-Policy',
-          value:
-            "default-src 'self'; " +
-            "base-uri 'self'; " +
-            "object-src 'none'; " +
-            "connect-src 'self' https://dms.genaitech.dev; " +
-            "img-src 'self' data: https:; " +
-            "script-src 'self' 'unsafe-inline'; " +
-            "style-src 'self' 'unsafe-inline' https:; " +
-            "font-src 'self' https: data:; " +
-            "frame-src https://dms.genaitech.dev; " +
-            "frame-ancestors 'none';"
-        },
+        { key: 'Content-Security-Policy', value:
+          "default-src 'self'; base-uri 'self'; object-src 'none'; connect-src 'self' https://dms.genaitech.dev; img-src 'self' data: https:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https:; font-src 'self' https: data:; frame-ancestors 'none';" },
         { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
       ]
     }
   ]
 }
-
 
 };
 
